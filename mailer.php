2a3a41fa01cdd0c896babb10658f8adacbde9c01
shalini0316 <shalini.jay1603@gmail.com>
2020-07-09 08:58:26 +0530

    php file added

--- mailer.php
+++ mailer.php
@@ -8,7 +8,7 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://eathunt.epizy.com//index.php?success=-1#form");
+        header("Location: https://shalini0316.github.io/omnifood//index.php?success=-1#form");
         exit;
     }
 
@@ -30,6 +30,6 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: http://eathunt.epizy.com/index.php?success=1#form");
+    header("Location: https://shalini0316.github.io/omnifood/index.php?success=1#form");
 
 ?>
